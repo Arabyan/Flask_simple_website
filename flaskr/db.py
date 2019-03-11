edabf051a3c8ecac19f1096ad7ae8eb4dc9412ff
Arabyan <grigorarabyan@yahoo.com>
2019-03-11 08:30:26 +0400

    added init_app function

--- flaskr/db.py
+++ flaskr/db.py
@@ -32,3 +32,7 @@ def init_db_command():
     """Clear the existing data and create ne tables."""
     init_db()
     click.echo("Initialized the datebase.")
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
