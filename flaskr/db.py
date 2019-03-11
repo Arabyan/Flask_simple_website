8cb2b343f3921ff59c15a3338de3e15028bd1948
Arabyan <grigorarabyan@yahoo.com>
2019-03-11 08:58:48 +0400

    Define and Acces the Datebase

--- flaskr/db.py
+++ flaskr/db.py
@@ -8,7 +8,8 @@ def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATEBASE'],
-            detect_typles = sqlite3.PARSE_DECLTYPES
+            detect_types = sqlite3.PARSE_DECLTYPES
+
         )
         g.db.row_factory = sqlite3.Row
 
@@ -20,18 +21,22 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+
 @click.command('init-db')
 @with_appcontext
+
+
 def init_db_command():
-    """Clear the existing data and create ne tables."""
+    """Clear the existing data and create new tables."""
     init_db()
-    click.echo("Initialized the datebase.")
+    click.echo('Initialized the database.')
 
 def init_app(app):
     app.teardown_appcontext(close_db)
